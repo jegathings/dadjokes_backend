9d1ce4fd780d18c495c32dde7d8489356b57cd60
Ian Galleher <ian.galleher@gmail.com>
2020-06-09 15:28:10 -0500

    updated routes for heroku in controllers/dadjokes.js

--- models/dadjokes.js
+++ models/dadjokes.js
@@ -1,7 +1,7 @@
 const { Schema, model } = require('mongoose');
 
 const dadJokeSchema = Schema({
-    joke: {type: String, required: true}
+    joke: {type: String, required: true, distinct: true}
 });
 
 const DadJoke = model('dadjoke', dadJokeSchema);
