64c8e7e97ad7dc8822aba6d5eca12d665f77e065
Ian Galleher <ian.galleher@gmail.com>
2020-06-10 15:04:04 -0500

    fixed models/dadjokes.js to add unique index to Schema to prevent duplicate jokes in collection

--- models/dadjokes.js
+++ models/dadjokes.js
@@ -1,8 +1,9 @@
 const { Schema, model } = require('mongoose');
 
 const dadJokeSchema = Schema({
-    joke: {type: String, required: true, distinct: true}
+    joke: {type: String, required: true, unique: true}
 });
+dadJokeSchema.index( {'joke' : 1 }, {unique : true });
 
 const DadJoke = model('dadjoke', dadJokeSchema);
 
