9dfbca6cae90189644e1c0001ea4965768db4f21
Ian Galleher <ian.galleher@gmail.com>
2020-06-13 17:13:23 -0500

    updated Schema to include rating, /models/dadjokes.js

--- models/dadjokes.js
+++ models/dadjokes.js
@@ -2,7 +2,8 @@ const { Schema, model } = require('mongoose');
 
 const dadJokeSchema = Schema({
     setup: {type: String, required: true, unique: true},
-    punchline: {type: String, required: true, unique: true}
+    punchline: {type: String, required: true, unique: true},
+    rating: {type: String, required: true, default: "1"}
 });
 dadJokeSchema.index( {'setup' : 1, 'punchline': 1 }, {unique : true });
 
